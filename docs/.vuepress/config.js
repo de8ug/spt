4a21bd6db5314b06c8520bd58eb431fd523558a1
de8ug <de8ug@foxmail.com>
2018-09-22 17:51:36 +0800

    add git repo edit & modify upper case

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,6 +11,23 @@ module.exports = {
   themeConfig: {
     sidebarDepth: 2, // 将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
     lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间,
+    repo: 'de8ug/spt',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: '查看源码',
+
+    // 以下为可选的编辑链接选项
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: 'de8ug/spt',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'master',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！',
     nav:[
       { text: 'side1', link: '/side1/' }, // 内部链接 以docs为根目录
       { text: '51', link: 'http://edu.51cto.com' }, // 外部链接
